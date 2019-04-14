f280de9c3c11f6517a5ec5fed3abd4acb6ae87c3
Seth Millerd <millerd2seth@gmail.com>
2019-04-14 13:27:07 -0400

    pseudo code for accountsight

---
+++ module/accountsight.py
@@ -0,0 +1,28 @@
+#
+
+import pandas as pd
+import urllib
+
+
+class AccountSightInterface:
+    def __init__(self):
+        pass
+
+    def login(self, username, password):
+        """
+        method used for logging into the AccountSight website
+
+        :param username:
+        :param password:
+        :return:
+        """
+        pass
+
+    def submit_hours(self, report):
+        """
+        method used for submitting the report to accountsight
+
+        :param report: pandas.DataFrame 
+        :return:
+        """
+        pass
