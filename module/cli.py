86b426f53e6323e17259610fbccf3ae56f946c1d
Seth Millerd <millerd2seth@gmail.com>
2019-04-14 13:37:03 -0400

    pseudo code for cli

---
+++ module/cli.py
@@ -0,0 +1,22 @@
+#
+
+import argparse
+
+
+class AutoTSApp:
+    def __init__(self):
+        pass
+
+    def get_ts_from_hours(self):
+        pass
+
+    def submit_ts_to_accountsight(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
