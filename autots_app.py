6e0f759fc7ef55d415934139cbb9e6a612829820
Seth Millerd <millerd2seth@gmail.com>
2019-04-28 20:54:41 -0400

    transform dataframe, and allow for multiple retries on report download

--- autots_app.py
+++ autots_app.py
@@ -1,5 +1,6 @@
 #
 
+import pandas as pd
 import argparse
 import datetime
 import getpass
@@ -34,12 +35,67 @@ class AutoTSApp:
         if not hi.login(self.hoursforteams_username, password):
             raise ValueError("Unable to login, double check the password and try again")
 
-        summarized_report_dict = hi.download_summarized_report(self.start_timestamp, self.end_timestamp)
-        summarized_report_df = summarized_report_dict['data']
+        print('Login Success!')
+
+        # nasty bit of code that retries the report download 5 times. This is to handle the inconsistency
+        retries = 5
+        count = 0
+        summarized_report_dict, summarized_report_df = dict(), pd.DataFrame()
+        summarized_report_dict['fetch_export_url'] = False
+        while count < retries:
+            count += 1
+            try:
+                summarized_report_dict = hi.download_summarized_report(self.start_timestamp, self.end_timestamp)
+                summarized_report_df = summarized_report_dict['data']
+                if not summarized_report_dict['fetch_export_url']:
+                    raise ValueError('Failed to obtain export_url')
+                elif not summarized_report_dict['download_csv']:
+                    raise ValueError('Failed to obtain download_csv')
+                else:
+                    break
+            except ValueError:
+                pass
+
+        if not summarized_report_dict['fetch_export_url']:
+            raise ValueError('Failed to obtain export_ur: max retries exceeded')
+
         return summarized_report_df
 
-    def transform_and_agg_ts(self):
-        pass
+    @staticmethod
+    def transform_and_agg_ts(data):
+        """
+
+        :param data:
+        :return:
+        """
+        # change field names to AccountSight field names
+        hoursforteams_accountsight_key = {"Client": "Customer",
+                                          "Project": "Project",
+                                          "Date": "Date",
+                                          "Start": "Start Time",
+                                          "End": "End Time",
+                                          "Note": "Comments",
+                                          "Duration": "Hours",
+                                          "Person": "User ID/Email ID"
+                                          }
+
+        out_data = data.rename(hoursforteams_accountsight_key, axis=1)
+
+        # aggregate data into appropriate level for Accountsight
+        out_data = out_data.groupby(['Date', 'Customer', 'Project']).agg({'Hours': 'sum',
+                                                                          # 'Comments': 'sum'
+                                                                          })
+        out_data.reset_index(level=[0, 1, 2], inplace=True)
+
+        # transform date field into correct format for Accountsight
+        out_data['Date'] = out_data.apply(
+            lambda x: datetime.datetime.strptime(x['Date'], '%m/%d/%Y').strftime('%d-%b-%Y'),
+            axis=1)
+
+        # create null columns for blank fields in AccountSight upload
+        out_data = pd.concat([out_data, pd.DataFrame(columns=['Task', 'Start Time', 'End Time', 'User ID/Email ID'])],
+                             sort=False)
+        return out_data
 
     def submit_ts_to_accountsight(self):
         pass
@@ -67,7 +123,8 @@ def main():
 
     ats = AutoTSApp(args.start_date, args.end_date, args.hoursforteams_username)
     data = ats.get_ts_from_hours()
-    # print(data)
+    data_ready_for_upload = ats.transform_and_agg_ts(data)
+    print(data_ready_for_upload)
 
 
 if __name__ == "__main__":
